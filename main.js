846e76ab14a4a70cf30663e89bd84ef1fb445d10
ubai-d <ubaidurrehman14827@gmail.com>
2024-02-25 10:42:17 +0500

    class02 code pushed

---
+++ main.js
@@ -0,0 +1,6 @@
+var favouriteColor = "blue";
+console.log(favouriteColor);
+var greeting = "Hello World";
+console.log(greeting);
+var number = 12345;
+console.log(number);
---
+++ main.ts
@@ -0,0 +1,16 @@
+let favouriteColor = "blue"
+console.log(favouriteColor)
+
+let greeting = "Hello World"
+console.log(greeting)
+
+let number = 12345
+console.log(number)
+
+let camelCase = "camelCase"
+console.log(camelCase)
+
+let snake_case = "snake_case"
+console.log(snake_case)
+
+let PascalCase = "PascalCase"
